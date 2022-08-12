5e344980fd3ad3be057a469b9ff8486ef028a9a6
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-08-12 13:02:12 +0530

    Modified
    Modified

--- sefty-client/src/app/home/admin/role-management/role.module.ts
+++ sefty-client/src/app/home/admin/role-management/role.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RoleManagementComponent } from './role-management.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [RoleManagementComponent],
   imports: [
     CommonModule
   ]
--- sefty-client/src/app/home/admin/user-management/user.module.ts
+++ sefty-client/src/app/home/admin/user-management/user.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserManagementComponent } from './user-management.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserManagementComponent],
   imports: [
     CommonModule
   ]
