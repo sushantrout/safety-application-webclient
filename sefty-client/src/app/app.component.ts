1085100e9a86ffb293536a7ef119b975139809ec
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-08-12 11:55:32 +0530

    Initial Commit
    Initial Commit

---
+++ sefty-client/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  title = "sefty-client";
+
+  cities: City[];
+
+  selectedCity!: City;
+
+  constructor() {
+    this.cities = [
+      { name: "New York", code: "NY" },
+      { name: "Rome", code: "RM" },
+      { name: "London", code: "LDN" },
+      { name: "Istanbul", code: "IST" },
+      { name: "Paris", code: "PRS" },
+    ];
+  }
+}
+
+interface City {
+  name: string;
+  code: string;
+}
