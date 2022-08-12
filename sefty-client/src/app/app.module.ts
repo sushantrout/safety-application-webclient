1279462abfa2a66c5a9f10ab3881ebcdac8b5f59
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-08-12 12:03:48 +0530

    Template
    Template

--- sefty-client/src/app/app.module.ts
+++ sefty-client/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SelectButtonModule} from 'primeng/selectbutton';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { RoleManagementComponent } from './admin/role-management/role-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    UserManagementComponent,
+    RoleManagementComponent
   ],
   imports: [
     BrowserModule,
