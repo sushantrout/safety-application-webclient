523b390bc2ce66e6258b5d11cb72905d32a07d7d
Rahul Anand <rahul.anand435@gmail.com>
2022-08-16 14:58:48 +0530

    rahul--login,sign-up component created
    rahul--login,sign-up component created

--- sefty-client/src/app/login/login.component.ts
+++ sefty-client/src/app/login/login.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  showLogin: boolean = true;
+  showSignup: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  loadSignUp() {
+    this.showLogin = false;
+    this.showSignup = true;
+  }
+  loadLogin() {
+    this.showSignup = false;
+    this.showLogin = true;
+  }
 }
--- sefty-client/src/app/shared/shared.module.ts
+++ sefty-client/src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SelectButtonModule } from 'primeng/selectbutton';
 import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { TranslateModule } from '@ngx-translate/core';
+
 @NgModule({
   declarations: [],
-  imports: [CommonModule, SelectButtonModule, CardModule],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule, SelectButtonModule, CardModule],
+  imports: [CommonModule, CardModule, ButtonModule, TranslateModule],
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, CardModule, ButtonModule, TranslateModule],
 })
 export class AppSharedModule { }
